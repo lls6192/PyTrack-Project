f90dc02c496c936cb7b143a0930c3fb02f1c00a9
lexisung <lls6192@rit.edu>
2026-03-17 20:00:16 -0400

    made start up page

---
+++ startup_page.py
@@ -0,0 +1,14 @@
+from tkinter import *
+
+def create_new_location():
+    # Code to create a new location
+    pass
+
+# Create Window
+root_window = Tk()
+root_window.title("PyTrack")
+root_window.geometry("400x300")
+
+Label(root_window, text="Welcome to PyTrack!").pack(pady=20)
+
+root_window.mainloop()
