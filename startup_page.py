5f3108912e6f7d85276b39a8200db1b4cb897462
lexisung <lls6192@rit.edu>
2026-03-18 14:25:14 -0400

    made starter page GUI

--- startup_page.py
+++ startup_page.py
@@ -16,11 +16,18 @@ root_window = Tk()
 root_window.title("PyTrack")
 root_window.geometry("400x300")
 
-# Title
-title = Label(root_window, text="Welcome to PyTrack!", font=("Arial", 24)).pack(pady=20)
+# Make columns expand (for centering)
+root_window.columnconfigure(0, weight=1)
+root_window.columnconfigure(1, weight=1)
 
+# Title
+title = Label(root_window, text="Welcome to PyTrack!", font=("Arial", 24))
+title.grid(row=0, column=0, columnspan=2, pady=20, sticky="ew")
+
+#------------------------------------------------------------------
 # Label for Selecting an Existing Location
-Label(root_window, text="Select an Existing Location:").pack()
+lbl_existing_location = Label(root_window, text="Select an Existing Location:")
+lbl_existing_location.grid(row=1, column=0, padx=40, pady=1, sticky="w")
 
 # Dropdown options  
 existing_locations = ["Webster", "Pittsford", "Henrietta"]
@@ -28,10 +35,16 @@ existing_locations = ["Webster", "Pittsford", "Henrietta"]
 # Combobox  
 cb = ttk.Combobox(root_window, values=existing_locations)
 cb.set("Select a location")
-cb.pack()
+cb.grid(row=2, column=0, padx=40, pady=1, sticky="w")
 
 # Button to display selection  
-Button(root_window, text="Submit", command = cmd_submit).pack()
+btn_submit = Button(root_window, text="Submit", command = cmd_submit)
+btn_submit.grid(row=3, column=0, padx=40, pady=1, sticky="w")
+#------------------------------------------------------------------
+
+# Button to create a new location
+btn_create_new_location = Button(root_window, text="Create New Location", command=create_new_location)
+btn_create_new_location.grid(row=2, column=1, padx=5)
 
 root_window.mainloop()
 
