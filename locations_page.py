92f6586f25d877ee1c950de84b571e7659389bf2
lexisung <lls6192@rit.edu>
2026-03-24 19:40:36 -0400

    changed locatations title

--- locations_page.py
+++ locations_page.py
@@ -7,11 +7,8 @@ class LocationPage(ttk.Frame):
 
         self.controller = controller
 
-        label = Label(self, text=f"Store Location Page", font=("Times New Roman", 24))
-        label.pack(pady=20)
-
-        self.location_label = Label(self, text="", font=("Times New Roman", 16))
-        self.location_label.pack(pady=10)
+        self.title_label = Label(self, text=f"Store Location Page ", font=("Times New Roman", 24))
+        self.title_label.pack(pady=20)
 
         btn_back = Button(
             self,
@@ -21,4 +18,5 @@ class LocationPage(ttk.Frame):
         btn_back.pack()
 
     def set_location(self, location_name):
-        self.location_label.config(text=f"Selected: {location_name}")
+        # Update title
+        self.title_label.config(text=f"Store Location Page - {location_name}" )
